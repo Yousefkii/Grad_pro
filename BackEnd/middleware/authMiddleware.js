740f94b553671af113766dd131e3ab4d7b21a767
Yousefkii <youssefhegaze908@gmail.com>
2025-12-13 00:45:56 +0100

    _Fetching Dashboard API _ fixing an Error giving an error in the netwrok {no token, unauthrizied token}

--- BackEnd/middleware/authMiddleware.js
+++ BackEnd/middleware/authMiddleware.js
@@ -1,15 +1,15 @@
 const jwt = require("jsonwebtoken");
-const user = require("../models/User");
+const User = require("../models/User");
 
 exports.protect = async (req, res, next) =>{
-    let token = req.headers.authorization?.split("")[1];
+    let token = req.headers.authorization?.split(" ")[1];
     if(!token){
         return res.status(400).json({message: "Not authorized, no token"});
     }
 
     try{
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = await User.findById(decoded.id).select('._password');
+        req.user = await User.findById(decoded.id).select('-password');
             next();
         
     }catch (err){
--- FrontEnd/expensesTracker/src/pages/Auth/Dashboard/Home.jsx
+++ FrontEnd/expensesTracker/src/pages/Auth/Dashboard/Home.jsx
@@ -1,14 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DashboardLayout from '../../../components/layouts/DashboardLayout'
 import { UserContext } from '../../../context/UserContext'
 import { UseUserAuth } from '../../../hooks/UseUserAuth';
+import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../../../utils/axiosinstance';
+import { API_PATHS } from '../../../utils/apiPaths';
+import { useEffect } from 'react';
 
 const Home = () => {
   UseUserAuth();
+
+  const navigate = useNavigate();
+
+  const [dashboardData, setDashboardData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+const fetchDashboardData = async ()=>{
+  if(loading ) return ;
+
+  setLoading(true);
+  try {
+    const response = await axiosInstance.get(`${API_PATHS.DASHBOARD.GET_DATA}`);
+
+    if(response.data){
+      setDashboardData(response.data);
+    }
+  
+}catch (error){
+  console.log("something went wrong, please try again later", error);
+}finally {
+  setLoading(false);
+};}
+
+useEffect(() => {
+fetchDashboardData();
+  return () => {
+  }
+}, [])
+
+
   return (
     <DashboardLayout activeMenu = "Dashboard">
     <div className='my-5 mx-auto'>
-Home
+
     </div>
     </DashboardLayout>
   )
